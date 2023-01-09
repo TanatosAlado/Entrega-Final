25368a7876bba5c7c3f65f88a1f374f9da49947c
Cristian Alessandria <cristian_alessandria@hotmail.com>
2023-01-08 22:39:43 -0300

    Instalando dependencias

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+Tengo 3 usuarios registrados:
+    - urs: admin1@gmail.com
+    - pass: Admin1
+
+    - urs: admin2@gmail.com
+    - pass: Admin2
+
+    - urs: cris@gmail.com
+    - pass: Cristian
+
+
+Consideraciones mensajes: 
+- Por cada nuevo mensaje en el chat, se crea automaticamente un guion ( - ) en el campo respuesta, indicando que aún no fue respondida.
---
+++ server.js
@@ -0,0 +1,298 @@
+const express=require("express");
+const{Server:http}=require ("http");
+const {Server:ioServer}=require ("socket.io");
+const User=require("./src/schema/schemaUser.js")
+const session =require("express-session")
+const MongoStore=require("connect-mongo");
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const { comparePassword, hashPassword } = require("./src/services/utils")
+const { Types } = require("mongoose");
+// const {saveMsjs, getMsjs, sendWhatsapp, sendMail, sendSms,deleteCartBuy}=require ("./src/controllers/mensajes.js")
+const {saveMsjs, getMsjs, sendWhatsapp, sendMail, sendSms,deleteCartBuy}=require ("./public/js/send")
+const nodemailer= require('nodemailer');
+const { argv0 } = require("process");
+const { db } = require("./src/schema/schemaProducts.js");
+
+const {routerCarrito} = require("./src/routes/carros")
+const {routerProducto} = require("./src/routes/productos")
+
+
+//="="="="="=""="=="="="==""="
+
+const {
+  loggerDev,
+  loggerProd
+} = require("./logger_config");
+
+const NODE_ENV = process.env.NODE_ENV || "development";
+const logger = NODE_ENV === "production"
+? loggerProd
+: loggerDev
+
+const cluster = require("cluster");
+const {cpus} = require('os');
+const cons = require("consolidate");
+const cpuNum = cpus().length;
+
+const modoCluster = false;
+
+console.log(`node ${NODE_ENV}`)
+
+if(modoCluster){
+  console.log("Se iniciará en modo CLUSTER")
+}
+else{
+  console.log("Se iniciará en modo FORK")
+}
+
+if (modoCluster && cluster.isPrimary) {
+  console.log(`Cluster iniciado. CPUS: ${cpuNum}`);
+  console.log(`PID: ${process.pid}`);
+  for (let i = 0; i < cpuNum; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", worker => {
+    console.log(`${new Date().toLocaleString()}: Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  const httpserver = http(app)
+  const io = new ioServer(httpserver)
+
+
+//----------
+var engines= require("consolidate")
+app.engine("html", engines.swig)
+app.set("view engine", "html")
+//--------------
+  
+  app.use("/public", express.static('./public/'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use('/productos', routerProducto);
+  app.use('/carritos', routerCarrito);
+  // app.use('/mensajes', routerMensajes);
+  
+
+  app.use(session({
+    secret: 'TanatosAlado',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+      mongoUrl:process.env.URL_BD,
+      retries: 0,
+      ttl: process.env.TIME,
+    }),
+  })
+  );
+  
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+   //   //RECUPERO EL NOMBRE YA EN SESION INICIADA
+   app.get('/loginEnv', (req, res) => {
+    process.env.USER=req.user.name;
+    process.env.avatar=req.user.avatar;
+    const user = process.env.USER;
+    const avatar=process.env.avatar;
+    res.send({
+        user,avatar
+    })
+    
+  })
+  
+  
+   //   //RECUPEROel ID DeL CARRO EN SECION INICIADA
+   app.get('/idCart', (req, res) => {
+    process.env.USER=req.user.name;
+    process.env.id=req.user.id;
+    process.env.avatar=req.user.avatar
+    const user = process.env.USER;
+    const id=process.env.id
+    const avatar=process.env.avatar
+    res.send({
+        user,id,avatar
+    })
+    
+  })
+  
+  
+  //RECUPERO EL NOMBRE YA EN SESION INICIADA
+  app.get('/getUserNameEnv', (req, res) => {
+    const user = process.env.USER;
+    
+      res.send({
+        user
+    })
+  })
+  
+  app.get("/", (req,res)=>{
+  
+      try{
+          if (req.session.user){
+             res.sendFile(__dirname + ('/public/index.html'))
+             logger.log("info",`Ingreso a la ruta${req.url}`)
+          }
+          else
+          {
+              res.sendFile(__dirname + ('/views/login.html'))
+              logger.log("info",`Ingreso a la ruta${req.url}`)
+          }
+      }
+      catch (error){
+       console.log(error)
+      }
+  
+  })
+  
+  io.on('connection', async (socket) => {
+      console.log('Usuario conectado');
+      socket.on('enviarMensaje', (msj) => {
+          saveMsjs(msj);
+      })
+  
+      socket.emit ('mensajes', await getMsjs());
+  })
+  
+  // // DESLOGUEO DE USUARIO
+  
+  app.get('/logout', (req, res) => {
+      try {
+          req.session.destroy((err) => {
+              if (err) {
+                  console.log(err);
+              } else {
+                  res.redirect('/logout');
+                  logger.log("info",`Ingreso a la ruta${req.url}`)
+              }
+          })
+      } catch (err) {
+          console.log(err);
+      }
+  })
+  app.get('/logoutMsj', (req, res) => {
+      try {
+          res.sendFile(__dirname + '/views/logout.html');
+          logger.log("info",`Ingreso a la ruta${req.url}`)
+      }
+      catch (err) {
+          console.log(err);
+      }
+  })
+
+//-------------------------------
+
+app.get('/buyCart', async(req, res) => {
+  try{
+  process.env.USER=req.user.mail;
+  process.env.id=req.user.id;
+  process.env.name=req.user.name
+  process.env.phone=req.user.phone
+  // const idProductos=process.env.id
+   const id=parseInt( process.env.id)
+
+  const productos=await db.collection("carts").findOne({id:id})
+  console.log(productos)
+  const mail = process.env.USER;
+  const phone=process.env.phone
+  const name= process.env.name
+       sendWhatsapp(name,mail)
+       sendMail(name,mail,JSON.stringify(productos))
+       sendSms(phone)
+       deleteCartBuy(id)
+  res.redirect("/buySuccessfull")
+  logger.log("info",`Ingreso a la ruta${req.url}`)
+  
+
+   }
+  catch(err){
+    console.log(err)
+  }
+})
+
+//-------------------------------
+
+
+
+
+   
+    app.get("/login", (req, res) => {
+      res.sendFile(__dirname + "/views/login.html");
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+  
+    app.get("/signup", (req, res) => {
+      res.sendFile(__dirname + "/views/register.html");
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+  
+    app.get("/loginFail", (req, res) => {
+      res.sendFile(__dirname + "/views/loginFail.html");
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+  
+    app.get("/signupFail", (req, res) => {
+      res.sendFile(__dirname + "/views/signupFail.html");
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+    app.get("/cart", (req, res) => {
+      res.sendFile(__dirname + "/views/cart.html");
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+
+    app.get("/chat", (req, res) => {
+      res.sendFile(__dirname + "/views/messages.html");
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+  
+
+//---------------------------------------------------------
+
+    app.get("/buySuccessfull", (req, res) => {
+      res.sendFile(__dirname + "/views/buyCart.html");
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+
+//----------------------------------------------------------
+  
+    app.post("/signup", passport.authenticate("signup", {
+      failureRedirect: "/signupFail",
+    }) , (req, res) => {  
+      req.session.user = req.user;
+      res.redirect("/login");
+    });
+    
+    app.post("/login", passport.authenticate("login", {
+      failureRedirect: "/loginFail",
+    }) ,(req, res) => {
+        req.session.user = req.user;
+        res.redirect('/cart');
+    });
+
+    app.get("/info", (req, res) => {
+      res.render("infoServer.pug",{PID:process.pid,VERSION:process.version,MEMORIA:process.memoryUsage().rss,SISTEMAOPERATIVO:process.platform,CARPETA:process.cwd(),PATH:process.argv[0]});
+      logger.log("info",`Ingreso a la ruta${req.url}`)
+    });
+  
+    app.get("*", (req, res) => {
+      logger.log("warn",`Ruta no encontrada ${req.url}`)
+      res.status(400).send(`Ruta no encontrada ${req.url}`);
+    });
+  
+    const PORT = process.env.PORT;
+  
+    const server = httpserver.listen(PORT, () => {
+        console.log(`Server is running on port: ${server.address().port}`);
+    });
+    server.on('error', error => console.log(`error running server: ${error}`));
+  
+  
+}
+
+
+//="="="="="=""="=="="="==""="
+
